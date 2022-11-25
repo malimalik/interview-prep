fccc4fe40034bb4e06198d789998f8d26ef62918
Ali <malik99@yorku.ca>
2022-11-25 12:59:59 -0500

    Add question description to inordertraversal

--- treesandgraphs/InOrderTraversal.java
+++ treesandgraphs/InOrderTraversal.java
@@ -1,13 +1,40 @@
 package treesandgraphs;
 
-public class InOrderTraversal {
-    List<Integer> result = new ArrayList<>();
+/*
+ * Given the root of a binary tree, return the inorder traversal of its nodes' values.
 
-    public List<Integer> inorderTraversal(TreeNode root) {
-      if (root == null) return new ArrayList<Integer>();
-      inorderTraversal(root.left);
-      result.add(root.val);
-      inorderTraversal(root.right);
-      return result;
-    }
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ */
+
+public class InOrderTraversal {
+
+  List<Integer> result = new ArrayList<>();
+
+  public List<Integer> inorderTraversal(TreeNode root) {
+    if (root == null) return new ArrayList<Integer>();
+    inorderTraversal(root.left);
+    result.add(root.val);
+    inorderTraversal(root.right);
+    return result;
+  }
 }
